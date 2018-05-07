0ec99a2a60827154485d7dd343417f0eafd4b6e5
Juan Pablo Arevalo <juan.arevalo.merchan@hotmail.com>
2018-05-06 20:30:32 -0500

    Init

---
+++ app/src/main/java/cosw/eci/edu/androidchat/ui/controller/ChatActivity.java
@@ -0,0 +1,17 @@
+package cosw.eci.edu.androidchat.ui.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import cosw.eci.edu.androidchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+}
